e62c681a56caca3d0816bed0175a37bc1ecd8635
Demo User <demouser@m190583.local>
2011-11-05 16:34:05 -0400

    first pass at ParkingCalculator

--- src/ParkingCalculator.java
+++ src/ParkingCalculator.java
@@ -1,20 +1,38 @@
 import java.util.ArrayList;
-
+import java.util.Date;
 
 public class ParkingCalculator {
 
+        ArrayList<User> existingUsers;
+        ArrayList<ParkingLocation> locations;
+        
 	public ParkingCalculator(){
 		
 	}
 	
 	private void determineBestSpots(ArrayList<User> newUsers){
-		for (User u : newUsers){
-			recommendSpots(u);
-			//push spots to the user 
-		}
+            //new users
+            for (User u : newUsers){
+                // get k closest spots
+                ArrayList<ParkingLocation> locations = recommendSpots(u);
+		for (ParkingLocation location : locations) {
+                    
+                }
+                // get best three
+                // update u
+                //push spots to the user 
+            }
+            
+            //existing users
+            for (User existing : existingUsers) {
+                // update p
+                // update u
+                // push updates to the user
+            }
 		
 	}
 	
+        // get
 	private ArrayList<ParkingLocation> recommendSpots(User user){
 		
 		ArrayList<ParkingLocation> topThreeSpots = new ArrayList<ParkingLocation>();
@@ -26,4 +44,26 @@ public class ParkingCalculator {
 		
 		return topThreeSpots; 
 	}
+        
+        private double getProbability(User u, ParkingLocation p) {
+            double wDistance = 1;
+            double wSpots = 1;
+            double wLikelyhood = 1;
+            
+            double distance = Distance(u.getLongitude(), u.getLatitude, p.getLongitude(), p.getLatitude());
+            double spots = p.getSpots();
+            double likelyhood = p.getLikelyhood();
+            double noise = Noise(new Date(), u.getLongitude(), u.getLatitude());
+            
+            double prob = wDistance * distance + wSpots * spots + wLikelyhood * likelyhood + noise;
+            return prob;
+            
+        }
+        
+        
+        private double Noise(Date date, double longitude, double latitude) {
+            return 1.0;
+        }
+ 
+        
 }
