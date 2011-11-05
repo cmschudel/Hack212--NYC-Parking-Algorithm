0cc9d8d9812cfe43047f4bc2c16190b3448c593c
Cecilia Schudel <cmschudel@gmail.com>
2011-11-05 16:13:36 -0400

    Updated the User with getters/setters.

--- src/User.java
+++ src/User.java
@@ -15,7 +15,7 @@ public class User {
 	 * @param latitude
 	 * @param time
 	 */
-	public User(long longitude, long latitude, Date time){
+	public User(long longitude, long latitude){
 		this.longitude = longitude;
 		this.latitude = latitude;
 		
@@ -24,6 +24,49 @@ public class User {
 		userid = UUID.randomUUID();
 	}
 	
+	/**
+	 * Initialize a user with a location and the time that they've requested a spot.
+	 * @param longitude
+	 * @param latitude
+	 * @param time
+	 */
+	public User(long longitude, long latitude, Date time){
+		this.longitude = longitude;
+		this.latitude = latitude;
+		
+		// Set the time of the user's request to the current time.
+		this.time = time;
+		userid = UUID.randomUUID();
+	}
+	
+	public long getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(long longitude) {
+		this.longitude = longitude;
+	}
+
+	public long getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(long latitude) {
+		this.latitude = latitude;
+	}
+
+	public Date getTime() {
+		return time;
+	}
+
+	public void setTime(Date time) {
+		this.time = time;
+	}
+
+	public UUID getUserid() {
+		return userid;
+	}
+
 	/**
 	 * Sets the collection of a User's k locations. 
 	 * @param newLocations
