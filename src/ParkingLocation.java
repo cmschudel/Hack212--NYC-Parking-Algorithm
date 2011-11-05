8fcd2e1cfba250823623e86e87817a5b5fbaab45
Cecilia Schudel <cmschudel@gmail.com>
2011-11-05 16:15:40 -0400

    Updated ParkingLocation with getters/setters.

--- src/ParkingLocation.java
+++ src/ParkingLocation.java
@@ -12,16 +12,48 @@ public class ParkingLocation {
 		this.spotsAvailable = 0;
 		this.likelihood = 0.0;
 	}
-
+	
 	public ParkingLocation(long longitude, long latitude, double likelihood, int spotsAvailable){
 		this.longitude = longitude;
 		this.latitude = latitude;
 		this.likelihood = likelihood;
 		this.spotsAvailable = spotsAvailable;
 	}
+
+	public long getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(long longitude) {
+		this.longitude = longitude;
+	}
+
+	public long getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(long latitude) {
+		this.latitude = latitude;
+	}
+
+	public int getSpotsAvailable() {
+		return spotsAvailable;
+	}
+
+	public void setSpotsAvailable(int spotsAvailable) {
+		this.spotsAvailable = spotsAvailable;
+	}
+
+	public double getLikelihood() {
+		return likelihood;
+	}
+
+	public void setLikelihood(double likelihood) {
+		this.likelihood = likelihood;
+	}
 	
-	public long longitude;
-	public long latitude;
-	public int spotsAvailable;
-	public double likelihood;
+	private long longitude;
+	private long latitude;
+	private int spotsAvailable;
+	private double likelihood;
 }
